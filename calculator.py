85337dd42cecf44dc0986c45221554f2a0a730b3
Divyansh Barar <68277579+divyanshbarar@users.noreply.github.com>
2020-11-25 22:52:17 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -29,13 +29,7 @@ def minus():
     f_num=int(first_number)
     e.delete(0,END)
 
-def multiply():
-    first_number=e.get()
-    global f_num
-    global math
-    math="multiply"
-    f_num=int(first_number)
-    e.delete(0,END)
+
 
 def divide():
     first_number=e.get()
